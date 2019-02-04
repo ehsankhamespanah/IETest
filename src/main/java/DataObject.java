40778eb87afd61974f935de35a82f1e0661a23d2
Ehsan Khamespanah <e.khamespanah@ut.ac.ir>
2019-02-04 17:14:54 +0330

    The first commit

---
+++ src/main/java/DataObject.java
@@ -0,0 +1,19 @@
+
+public class DataObject {
+	int value1;
+	int value2;
+	
+	public void setValue1(int value1) {
+		this.value1 = value1;
+	}
+	public void setValue2(int value2) {
+		this.value2 = value2;
+	}
+	public int getValue1() {
+		return value1;
+	}
+	
+	public int getValue2() {
+		return value2;
+	}
+}
